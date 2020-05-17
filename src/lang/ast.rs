8d9766e82a5916864502bb09e7da9a53adf484b8
Niklas Schöllhorn <schoellhorn.niklas@gmail.com>
2020-05-17 20:33:39 +0200

    (Quick) POC for an SQL AST in Rust

--- src/lang/ast.rs
+++ src/lang/ast.rs
@@ -1,4 +1,32 @@
-#[derive(Debug)]
-pub enum EventQL {
-    CreateTableStatement(String)
+#[derive(Debug, PartialEq)]
+pub(crate) enum EventQL {
+    CreateTableStatement {
+        table_identifier: String,
+        column_definitions: Vec<ColumnDefinition>,
+        topic_identifier: String,
+    },
 }
+
+#[derive(Debug, PartialEq)]
+pub(crate) struct ColumnDefinition {
+    pub(crate) identifier: String,
+    pub(crate) data_type: DataType,
+    pub(crate) schema_property_identifier: String
+}
+
+#[derive(Debug, PartialEq)]
+pub(crate) enum DataType {
+    Int,
+    String
+}
+
+impl DataType {
+    pub(crate) fn from_str(name: &str) -> DataType {
+        match name.to_lowercase().as_str() {
+            "int" => DataType::Int,
+            "string" => DataType::String,
+            _ => panic!("Unknown data type specified: {}", name),
+        }
+    }
+}
+
--- src/lang/main.rs
+++ src/lang/main.rs
@@ -2,9 +2,10 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
+use pest::iterators::{Pair, Pairs};
 use pest::Parser;
-use pest::iterators::{Pairs, Pair};
-use crate::ast::EventQL;
+
+use crate::ast::{EventQL, ColumnDefinition, DataType};
 
 mod ast;
 
@@ -13,27 +14,66 @@ mod ast;
 struct EventQLParser;
 
 fn main() {
-    let example_query = "CREATE TABLE someIdent; create TABLE anotherOne;";
+    let example_query = "CREATE TABLE someIdent (id INT FROM id, first_name STRING FROM fname, last_name STRING FROM lname) VIA sometopic; CREATE TABLE something (id INT FROM id, first_name STRING FROM fname, last_name STRING FROM lname) VIA othertopic;";
     println!("Parsing Query: {}", example_query);
     let parsed_query = EventQLParser::parse(Rule::eventQlStatement, example_query)
         .unwrap_or_else(|e| panic!("{}", e));
 
     let ast = create_ast(parsed_query);
-    println!("{:?}", ast);
+    println!("{:#?}", ast);
 }
 
 fn create_ast(parsed_query: Pairs<Rule>) -> std::vec::Vec<EventQL> {
-    let mut statements = vec!();
-    for statement in parsed_query {
-        statements.push(parse_statement(statement))
-    }
-
-    statements
+    parsed_query.map(|token| parse_statement(token))
+        .collect()
 }
 
 fn parse_statement(statement: Pair<Rule>) -> EventQL {
-    match statement.as_rule() {
-        Rule::createTableStatement => EventQL::CreateTableStatement(String::from(statement.into_inner().next().unwrap().as_str())),
-        _ => panic!("Unknown token: {:?}", statement.as_rule()),
+    let rule = statement.as_rule();
+    match rule {
+        Rule::createTableStatement => build_create_table_statement(statement.into_inner()),
+        _ => panic!("Unknown token: {:?}", rule),
+    }
+}
+
+fn build_create_table_statement(inner_tokens: Pairs<Rule>) -> EventQL {
+    let mut identifier: Option<String> = None;
+    let mut column_definitions: Vec<ColumnDefinition> = vec!();
+    let mut topic_identifier: Option<String> = None;
+
+    for token in inner_tokens {
+        match token.as_rule() {
+            Rule::identifier => identifier = Some(String::from(token.as_str())),
+            Rule::columnDefinition => column_definitions.push(build_column_definition(token.into_inner())),
+            Rule::schemaIdentifier => topic_identifier = Some(String::from(token.as_str())),
+            _ => ()
+        }
+    }
+
+    EventQL::CreateTableStatement {
+        table_identifier: identifier.expect("No table identifier specified."),
+        column_definitions,
+        topic_identifier: topic_identifier.expect("No topic identifier specified."),
+    }
+}
+
+fn build_column_definition(inner_tokens: Pairs<Rule>) -> ColumnDefinition {
+    let mut identifier: Option<String> = None;
+    let mut data_type: Option<DataType> = None;
+    let mut property_identifier: Option<String> = None;
+
+    for token in inner_tokens {
+        match token.as_rule() {
+            Rule::identifier => identifier = Some(String::from(token.as_str())),
+            Rule::columnType => data_type = Some(DataType::from_str(token.as_str())),
+            Rule::schemaIdentifier => property_identifier = Some(String::from(token.as_str())),
+            _ => ()
+        }
+    }
+
+    ColumnDefinition {
+        identifier: identifier.expect("No column identifier specified."),
+        data_type: data_type.expect("No data type specified."),
+        schema_property_identifier: property_identifier.expect("No property identifier specified.")
     }
 }
